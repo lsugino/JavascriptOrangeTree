b1ecfbaf6952730077ed18726733ad1eaa097e93
raorao <savinirs@gmail.com>
2013-11-06 21:19:29 -0800

    Revert "got all plain-old JS test working - marc, miya, brantley."
    This reverts commit 3079e67d6f8a48289861e8f881d0090f18438afc.

--- spec/javascripts/plain-old-js-objects-spec.js
+++ spec/javascripts/plain-old-js-objects-spec.js
@@ -1,41 +1,41 @@
 describe ("createTree", function() {
-  it("should return a tree object", function() {
+  xit("should return a tree object", function() {
     expect(createTree()).toBeDefined();
   });
 
-  it("should return tree with age 0 when created", function() {
+  xit("should return tree with age 0 when created", function() {
     var tree=createTree();
     expect(tree.age).toEqual(0);
   });
-  it("should have height 0 when created", function() {
+  xit("should have height 0 when created", function() {
     var tree=createTree();
     expect(tree.height).toEqual(0);
   });
-  it("should have 0 oranges when created", function() {
+  xit("should have 0 oranges when created", function() {
     var tree=createTree();
     expect(tree.orangeCount).toEqual(0);
   });
   describe("grow", function() {
 
-    it("should increase the age of the tree by 1 year", function() {
+    xit("should increase the age of the tree by 1 year", function() {
       var tree=createTree();
       tree.grow();
       expect(tree.age).toEqual(1);
     });
 
-    it("should increase the height of the tree by 10 inches", function() {
+    xit("should increase the height of the tree by 10 inches", function() {
       var tree=createTree();
       tree.grow();
       expect(tree.height).toEqual(10);
     });
-    it("should add a random number of oranges if age = FRUIT_BEARING_AGE", function() {
+    xit("should add a random number of oranges if age = FRUIT_BEARING_AGE", function() {
       var tree=createTree();
       while (tree.age < FRUIT_BEARING_AGE) {
         tree.grow();
       }
       expect(tree.orangeCount).toBeGreaterThan(0);
     });
-    it("should have 0 oranges if age < FRUIT_BEARING_AGE", function() {
+    xit("should have 0 oranges if age < FRUIT_BEARING_AGE", function() {
       var tree=createTree();
       while (tree.age < (FRUIT_BEARING_AGE-1)) {
         tree.grow();
@@ -45,7 +45,7 @@ describe ("createTree", function() {
   });
 
   describe ("die",function() {
-    it("should be alive when age <= MAX_AGE",function() {
+    xit("should be alive when age <= MAX_AGE",function() {
       var tree=createTree();
       while (tree.age < (MAX_AGE-1)) {
         tree.grow();
@@ -53,7 +53,7 @@ describe ("createTree", function() {
       tree.grow();
       expect(tree.isAlive).toEqual(true);
     });
-    it("should die when age > MAX_AGE",function() {
+    xit("should die when age > MAX_AGE",function() {
       var tree=createTree();
       while (tree.age < MAX_AGE) {
         tree.grow();
@@ -64,7 +64,7 @@ describe ("createTree", function() {
   });
 
   describe("dropOrange", function() {
-    it("should return the orange that is removed from oranges", function() {
+    xit("should return the orange that is removed from oranges", function() {
       var tree=createTree();
       while (tree.age< FRUIT_BEARING_AGE) {
         tree.grow();
@@ -75,11 +75,11 @@ describe ("createTree", function() {
 });
 
 describe ("createOrange", function() {
-  it("should return a orange object", function() {
+  xit("should return a orange object", function() {
     expect(createOrange()).toBeDefined();
   });
 
-  it("should return an orange with a random diameter > 0", function() {
+  xit("should return an orange with a random diameter > 0", function() {
     var orange = createOrange();
     expect(orange.diameter).toBeGreaterThan(0);
   });
